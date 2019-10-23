d2079f1cb3f5b9321d106f69cb3e38df5d7712bd
Paolo Notaro <paulstpr@gmail.com>
2019-10-23 11:20:40 +0200

    modularize training script

--- train.py
+++ train.py
@@ -21,8 +21,7 @@ precomputed_positive_class_weights = torch.tensor([9.41494656, 98.42056075, 17.8
 classification_thresholds = torch.tensor([0.5, 0.5, 0.5, 0.5, 0.5, 0.5]).to(device)
 
 
-if __name__ == '__main__':
-
+def load_data():
     ds_train, ds_test = produce_datasets('jigsaw-toxic-comment-classification-challenge/train.csv',
                                          'jigsaw-toxic-comment-classification-challenge/vocab.txt',
                                          split_ratio=test_ratio)
@@ -35,14 +34,23 @@ if __name__ == '__main__':
     else:
         positive_class_weights = compute_binary_median_frequency_balancing(ds_train)
     positive_class_weights = positive_class_weights.to(device)
+    return (train_loader, test_loader), positive_class_weights
 
+
+def load_model():
     print("Loading model...", end='')
-    model = LSTMMultiBinaryClassificationNet(num_embeddings=len(ds_train.vocab), embedding_dim=128, num_classes=6,
-                                             padding_idx=padding_idx, lstm_layers=2, hidden_size=512, p_dropout=0.5,
-                                             additional_fc_layer=128, dev=device)
-    model.to(device)
-    optimizer = Adam(model.parameters(), lr=lr)
+    lstm_model = LSTMMultiBinaryClassificationNet(num_embeddings=len(ds_train.vocab), embedding_dim=128,
+                                                  num_classes=6, padding_idx=padding_idx, lstm_layers=2,
+                                                  hidden_size=512, p_dropout=0.5, additional_fc_layer=None, dev=device)
+    net.to(device)
+    adam = Adam(model.parameters(), lr=lr)
     print('done.')
+    return lstm_model, adam
+
+
+def train_evaluate(loaders: tuple, model: torch.nn.Module, optimizer: torch.optim.Optimizer):
+
+    train_loader, test_loader = loaders
 
     print("Starting training...")
     writer = SummaryWriter()
@@ -63,7 +71,8 @@ if __name__ == '__main__':
 
             # forward
             output = model(tokens, input_lengths)
-            loss = -1/len(targets) * (positive_class_weights*targets*log(output) + (1 - targets)*log(1 - output)).sum()
+            loss = -1 / len(targets) * (
+                        positive_class_weights * targets * log(output) + (1 - targets) * log(1 - output)).sum()
             loss_value = loss.item()
 
             # backward
@@ -83,12 +92,11 @@ if __name__ == '__main__':
         print("\nEpoch completed in {:3.2f}s. Evaluating...\r".format(epoch_duration), end='')
         model.eval()
         test_loss = 0
-        total_correct = torch.zeros((6, ))
+        total_correct = torch.zeros((6,))
         total_true_positives = torch.zeros((6,))
         total_false_positives = torch.zeros((6,))
         total_real_positives = torch.zeros((6,))
         for j, ((tokens, targets), (input_lengths, _)) in enumerate(test_loader):
-
             # move to GPU
             tokens = tokens.to(device)
             targets = targets.float().to(device)
@@ -103,14 +111,15 @@ if __name__ == '__main__':
             total_true_positives += ((y_pred == 1) & (targets_b == 1)).sum(dim=0).cpu().float()
             total_false_positives += ((y_pred == 1) & (targets_b == 0)).sum(dim=0).cpu().float()
             total_real_positives += (targets_b == 1).sum(dim=0).cpu().float()
-            loss = -1/len(targets) * (positive_class_weights*targets*log(output) + (1 - targets)*log(1 - output)).sum()
+            loss = -1 / len(targets) * (
+                        positive_class_weights * targets * log(output) + (1 - targets) * log(1 - output)).sum()
             test_loss += loss.item()
 
         test_loss /= len(test_loader)
         accuracies = (total_correct.numpy() / len(ds_test))
         precisions = (total_true_positives / (total_true_positives + total_false_positives)).numpy()
         recalls = (total_true_positives / total_real_positives).numpy()
-        f1_scores = 2*precisions*recalls/(precisions + recalls)
+        f1_scores = 2 * precisions * recalls / (precisions + recalls)
 
         writer.add_scalar("Loss/val", test_loss, global_step=(epoch + 1) * len(train_loader))
         writer.add_embedding(mat=model.embedding.weight.data, metadata=ds_train.vocab.label_to_index.keys(),
@@ -123,3 +132,10 @@ if __name__ == '__main__':
         if test_loss < best_test_loss or avg_f1_score > best_f1_score:
             best_test_loss = test_loss
             torch.save(model, "loss={:.4f}_f1={:.4f}.pt".format(test_loss, avg_f1_score))
+
+
+if __name__ == '__main__':
+
+    data_loaders, class_weights = load_data()
+    model_, optimizer_ = load_model()
+    train_evaluate(data_loaders, model_, optimizer_)
